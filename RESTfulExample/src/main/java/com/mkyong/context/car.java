6c26d0d5ecbc74abeebe93c6fdfbadae96361ad6
heshanjse <heshanjayasinghe@Heshans-MacBook-Pro.local>
2015-05-20 20:39:36 +0530

    add restfulwebservice

---
+++ RESTfulExample/src/main/java/com/mkyong/context/car.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mkyong.context;
+
+/**
+ *
+ * @author heshanjayasinghe
+ */
+public class car {
+    private int id;
+    private String name;
+
+    public car(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
